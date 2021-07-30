1a31f11987ae635cd3de87fadaa63b56f2049117
nelson.Liu <nealson840123@gmail.com>
2021-07-30 09:40:48 +0800

    增加user schema和user case

---
+++ backend/src/models/user.ts
@@ -0,0 +1,33 @@
+import { IUsers } from '../types/user'
+import { model, Schema } from 'mongoose'
+import { number } from 'yargs'
+
+const UserSchema: Schema = new Schema(
+    {
+        _id: {
+            type: Number,
+            required: false
+        },
+        Name: {
+            type: String,
+            required: true
+        },
+        Passwd: {
+            type: String,
+            required: true
+        },
+        OwnPost_id:{
+            type: Array,
+            required: false
+        },
+        FollowPost_id: {
+            type: Array,
+            required: false
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export default model<IUsers>("Users", UserSchema)
