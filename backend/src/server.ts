439060bf616771d6ab8850ac972a28aa04967117
nelson.Liu <nealson840123@gmail.com>
2021-07-30 09:42:19 +0800

    增加3個API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,12 @@
 import fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
-import { Server, IncomingMessage, ServerResponse } from 'http'
+import { Server, IncomingMessage, ServerResponse, request } from 'http'
 import { establishConnection } from './plugins/mongodb'
 import { IQuestion } from './types/question'
+import { IUsers } from './types/user'
 
-import Users from './models/users'
-import Question from './models/question'
 import Cat from './models/cat'
-import Users from './models/Users'
-import Login from './models/login'
-import PostQuestion from './models/content'
+import Users from './models/user'
+import Question from './models/question'
 
 const server: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({
     logger: { prettyPrint: true }
@@ -26,124 +24,162 @@ const startFastify: (port: number) => FastifyInstance<Server, IncomingMessage, S
         establishConnection()
         const users = new Users([
             {
-            _id: 1,
-            Name: "Nelson",
-            Passwd:"12345",
+                _id: 1,
+                Name: "Nelson",
+                Passwd:"12345",
             },
             {
-            _id: 2,
-            Name: "Kevin",
-            Passwd: "678910",
+                _id: 2,
+                Name: "Kevin",
+                Passwd: "678910",
             }
         ],)
     })
 
-    server.get('/data', async (request: FastifyRequest, reply: FastifyReply) => {
+    server.get('/ping', async (request: FastifyRequest, reply: FastifyReply) => {
+        return reply.status(200).send({ msg: 'pong' })
+    })
+
+    server.get('/cats', async (request: FastifyRequest, reply: FastifyReply) => {
+        const cats = await Cat.find({}).exec()
+        return reply.status(200).send({ cats })
+    })
+
+    server.post('/cats', async (request: FastifyRequest, reply: FastifyReply) => {
+        const postBody = request.body
+        const cat = await Cat.create(postBody)
+        return reply.status(200).send({ cat })
+    })
+
+    //Create user api
+    server.post('/CreateUsers', async (request: FastifyRequest, reply:FastifyReply) =>{
+        const postBody: IUsers = request.body as IUsers
+        const c_users = await Users.create(postBody)
+        if(c_users === null){
+            return reply.status(201).send({msg: "Create Users Failed"})
+        }
+        else{
+            return reply.status(201).send({c_users})
+        }
+    })
+    
+    //Delete User Posts 回傳 userID,postID
+    server.delete('/deletePosts', async (request: FastifyRequest, reply: FastifyReply) => {
+        const deleteBody:any = request.body as any
+        const users:Array<IUsers> = await Users.find({ '_id':deleteBody.userID })
+        //Users.addToSet({})
+        //users[0].OwnPost_id.length
+        const result = await Users.findByIdAndRemove({
+            query:  { '_id':deleteBody.userID }
+        }) 
+        
+        return reply.status(200).send(users)
+    })
+
+    //Delete User
+    server.delete('/deleteUsers/:user_id', async (request: FastifyRequest, reply:FastifyReply) =>{
+        let param:any = request.params
+        const result = await Users.findByIdAndRemove(param.user_id).exec()
+        if(result != null){
+            return reply.status(200).send({ msg:'delete users success!' })
+        }
+        else{
+            return reply.status(200).send({ msg:'the user does not exist'})
+        }
+    })
+
+    //Modify User password
+    server.put('/user/:user_id/:user_passwd', async(request: FastifyRequest,reply:FastifyReply) => {
+        let param:any = request.params
+        const result = await Users.findByIdAndUpdate(
+        param.user_id,
+            {
+                'Passwd':param.user_passwd as string,
+            }
+        ).exec()
+
+        if(result != null){
+            return reply.status(200).send({ msg:'chage user password success! '})
+        }
+        else{
+            return reply.status(200).send({ msg:'password change fail' })
+        }
+    })
+
+    server.get('/getUsers', async (request: FastifyRequest, reply: FastifyReply) => {
+        const users: Array<IUsers> = await Users.find()
+        return reply.status(200).send({ users })
+    })
+
+    //login api
+    server.post('/loginData', async (request: FastifyRequest, reply: FastifyReply) => {
+        const postBody = request.body
+        const login = await Users.find({ postBody }).exec()
+        if(login != null)
+        {
+            return reply.status(200).send({ msg: 'login success!' })
+        }
+        else
+        {
+            return reply.status(200).send({ msg: 'account or password incorrect!' })
+        }
+    })
+
+    //question api
+    server.get('/getAllPost', async (request: FastifyRequest, reply: FastifyReply) => {
         const question: Array<IQuestion> = await Question.find()
         return reply.status(200).send({ question })
     })
 
-    server.post('/data', async (request: FastifyRequest, reply: FastifyReply) => {
+    server.post('/setNewQuestionToDB', async (request: FastifyRequest, reply: FastifyReply) => {
         const postBody: IQuestion = request.body as IQuestion
         const question = await Question.create(postBody)
         if(question === null)
-    
-        server.get('/cats', async (request: FastifyRequest, reply: FastifyReply) => {
-            const cats = await Cat.find({}).exec()
-            return reply.status(200).send({ cats })
-        })
-    
-        server.post('/cats', async (request: FastifyRequest, reply: FastifyReply) => {
-            const postBody = request.body
-            const cat = await Cat.create(postBody)
-            return reply.status(200).send({ cat })
-        })
-    
-    
-        //login api
-        server.post('/loginData', async (request: FastifyRequest, reply: FastifyReply) => {
-            const postBody = request.body
-            const login = await Login.find({ postBody }).exec()
-            if(login != null)
-            {
-                return reply.status(200).send({ msg: 'login success!' })
-            }
-            else
-            {
-                return reply.status(200).send({ msg: 'account or password incorrect!' })
-            }
-        })
-    
-        server.get('/content', async (request: FastifyRequest, reply: FastifyReply) => {
-            const question = await PostQuestion.find({}).exec()
-            return reply.status(200).send({ question })
-        })
-    
-        server.post('/content', async (request: FastifyRequest, reply: FastifyReply) => {
-            const postBody = request.body
-            const question = await PostQuestion.find({ postBody }).exec()
-            return reply.status(200).send({ question })
-        })
-    
-        //[測試] loginPage一般帳號密碼登入
-        //Input : account、password
-        //Output : loginMsg(login success! / password incorrect! / account not exist!) 、 User(Schema)
-        server.get('/loginData/:account/:password', async (request: FastifyRequest, reply: FastifyReply) => {
-            let param:any = request.params
-            let account = param.account
-            let password = param.password
-            console.log(account);
-            console.log(password);
-    
-            if(account === 'Daniel' && password === '1234')
-            {
-                return reply.status(200).send({ msg: 'login success!' })
-            }
-            else if(account === 'Daniel' && password != '1234')
+        {
+            return reply.status(201).send({msg: "Create Question Failed"})
+        }
+        else
+        {
+            return reply.status(201).send({ question })
+        }
+    })
+    server.post('/setNewAnswerToDB', async (request: FastifyRequest, reply: FastifyReply) => {
+        const postBody: IQuestion = request.body as IQuestion
+        const question = await Question.create(postBody)
+        if(question === null)
+        {
+            return reply.status(201).send({msg: "Create Question Failed"})
+        }
+        else
+        {
+            return reply.status(201).send({ question })
+        }
+    })
+    //[測試] loginPage一般帳號密碼登入
+    //Input : account/password
+    //Output : msg/userInfo
+    server.get('/loginData/:account/:password', async (request: FastifyRequest, reply: FastifyReply) => {
+        let param:any = request.params
+        let account = param.account
+        let password = param.password
+        console.log(account);
+        console.log(password);
 
-            {
-                return reply.status(201).send({msg: "Create Question Failed"})
-            }
-            else
-            {
-                return reply.status(201).send({ question })
-            }
-    
-        })
-    
-        //QuestionsListPage需要從DB中取出所有的問題用於顯示於列表之中
-        //Input : null
-        //Output : post(Schema)
-        server.get('/getAllPost', async (request: FastifyRequest, reply: FastifyReply) => {
-            
-        })
-    
-        //個別問題頁面，需要顯示出該問題底下所有的回覆，和該回覆者的Name
-        //Input : QuestionPK
-        //Output : Answer(Schema)/Name
-        server.get('/getAnswerByQuestionPK', async (request: FastifyRequest, reply: FastifyReply) => {
-            
-        })
-    
-        //透過UserPK查詢到使用者的Name
-        //Input : UserPK
-        //Output : Name
-        server.get('/getNameByUserPk', async (request: FastifyRequest, reply: FastifyReply) => {
-            
-        })
-    
-        //使用者發問問題，需要新增問題相關資訊至資料庫
-        //Input : UserPK、Contents、Question Type
-        //Output : success/fall
-        server.get('/setNewQuestionToDB', async (request: FastifyRequest, reply: FastifyReply) => {
-            
-        })
-    
-        //使用者回覆問題，需要新增回覆相關資訊至資料庫
-        //Input : UserPK、QuestionPK、Contents、Question Type
-        //Output : success/fall
-        server.get('/setNewAnswerToDB', async (request: FastifyRequest, reply: FastifyReply) => {
-            
+        if(account === 'Daniel' && password === '1234')
+        {
+            return reply.status(200).send({ msg: 'login success!' })
+        }
+        else if(account === 'Daniel' && password != '1234')
+        {
+            return reply.status(200).send({ msg: 'password incorrect!' })
+        }
+        else
+        {
+            return reply.status(200).send({ msg: 'account not exist!' })
+        }
+        
+
+        
     })
 
     return server
